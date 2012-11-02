13b8f84557fa2fc8c93c86a70988d5575db49e52
NGatti <gatti2602@gmail.com>
2012-11-02 00:40:06 -0300

    Sin Comentarios.- Mensaje automatizado

--- TrabajoPractico2/src/Principal.java
+++ TrabajoPractico2/src/Principal.java
@@ -1,11 +1,8 @@
 
 public class Principal {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hola Mundo");
 
 	}
 
