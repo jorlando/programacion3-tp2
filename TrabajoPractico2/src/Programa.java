e3eb86fb49d9e57328fcaeb4471b453cefb6c893
fede29 <fedee.29@hotmail.com>
2012-11-25 23:29:20 -0300

    ahora sale una ventana cuando corremos el programa, pero no hace nada...

--- TrabajoPractico2/src/Programa.java
+++ TrabajoPractico2/src/Programa.java
@@ -1,21 +1,21 @@
-
+import vista.VentanaPrincipal;
 import ar.uba.fi.algo3.titiritero.ControladorJuego;
 import ar.uba.fi.algo3.titiritero.vista.Ventana;
-import 
 
 public class Programa {
-	
+
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		 /*
-         * Instanciamos el mapa
-         */
-		
-		 /*
-         * Luego instancio los objetos del framework: una ventana y el controlador
-         */         
 		
 		ControladorJuego controlador = new ControladorJuego(true);
-		
+		Ventana ventana = new VentanaPrincipal(1024,768,controlador);
+		controlador.setSuperficieDeDibujo(ventana);
+        ventana.setVisible(true);
+        
+        controlador.setIntervaloSimulacion(20);
+		controlador.comenzarJuego();
 	}
 
 }
--- TrabajoPractico2/src/vista/VentanaPrincipal.java
+++ TrabajoPractico2/src/vista/VentanaPrincipal.java
@@ -1,12 +1,12 @@
-package Vista;
+package vista;
 
 import ar.uba.fi.algo3.titiritero.ControladorJuego;
 import ar.uba.fi.algo3.titiritero.vista.Ventana;
 
 public class VentanaPrincipal extends Ventana{
 	
-	public VentanaPrincipal(ControladorJuego unControlador) {
-        super(unControlador);
+	public VentanaPrincipal(int ancho, int alto, ControladorJuego unControlador) {
+        super(ancho,alto,unControlador);
         this.setTitle("Cop-Control");
 	}
 	
